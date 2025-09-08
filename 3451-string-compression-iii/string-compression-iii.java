aff00c3a9544de739fc203806de44c0edf271ccb
Mokhanes A <mokhanes007@gmail.com>
2025-09-08 11:25:34 +0530

    Time: 986 ms (12.90%) | Memory: 51.8 MB (7.10%) - LeetSync

---
+++ 3451-string-compression-iii/string-compression-iii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String compressedString(String word) {
+        int l = word.length();
+        String res ="";
+        int count  = 1;
+        for(int i = 0;i < l; i++){
+            if(i+1 < l && count < 9 && word.charAt(i) == word.charAt(i+1)){
+                count++;
+            }else{
+                res = res + count + word.charAt(i);
+                count = 1;
+            }
+        }
+
+        return res;
+    }
+}
