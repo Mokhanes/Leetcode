24753819aec00700415e6680cfba5962b5daa63c
Mokhanes A <mokhanes007@gmail.com>
2025-09-08 12:40:00 +0530

    Time: 13 ms (100.00%) | Memory: 46.1 MB (53.55%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.java
+++ 3451-string-compression-iii/string-compression-iii.java
@@ -1,17 +1,33 @@
 class Solution {
-    public String compressedString(String word) {
-        int l = word.length();
-        String res ="";
-        int count  = 1;
-        for(int i = 0;i < l; i++){
-            if(i+1 < l && count < 9 && word.charAt(i) == word.charAt(i+1)){
-                count++;
-            }else{
-                res = res + count + word.charAt(i);
-                count = 1;
-            }
-        }
+    // public String compressedString(String word) {
+    //     int l = word.length();
+    //     String res ="";
+    //     int count  = 1;
+    //     for(int i = 0;i < l; i++){
+    //         if(i+1 < l && count < 9 && word.charAt(i) == word.charAt(i+1)){
+    //             count++;
+    //         }else{
+    //             res = res + count + word.charAt(i);
+    //             count = 1;
+    //         }
+    //     }
 
-        return res;
+    //     return res;
+    // }
+    public String compressedString(String word) {
+        char[] chars = word.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<chars.length;){
+            char s=chars[i];            
+            i++;
+            int j=1;
+            while(i<chars.length && chars[i]==s && j<9){
+                j++;
+                i++;
+            }                          
+            sb.append((char) (j + '0'));          
+            sb.append(s);                    
+        }        
+        return sb.toString();
     }
 }
