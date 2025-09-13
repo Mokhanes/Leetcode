516026c55de935eae3ee598fa2e2660f87fa5b0c
Mokhanes A <mokhanes007@gmail.com>
2025-09-13 15:12:27 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (72.98%) - LeetSync

--- 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
@@ -1,30 +1,45 @@
 class Solution {
-    public int balancedStringSplit(String s) {
-        StringBuilder sb = new StringBuilder();
-        int count = 0;
-        sb.append(s.charAt(0));
-        for(int i =1;i<s.length();i++){
-            char c = s.charAt(i);
-            sb.append(c);
-            if(chack(sb.toString())){
-                count++;
-                sb.setLength(0);
+        public int balancedStringSplit(String s) {
+            int x = 0;
+            int count = 0;
+            int n = s.length();
+            for(int i=0;i<n;i++)
+            {
+                char ch=s.charAt(i);
+                if(ch == 'L') x++;
+                if(ch == 'R') x--;
+                if(x==0) count++;
             }
+            return count;
+
+        }
+
+    // public int balancedStringSplit(String s) {
+    //     StringBuilder sb = new StringBuilder();
+    //     int count = 0;
+    //     sb.append(s.charAt(0));
+    //     for(int i =1;i<s.length();i++){
+    //         char c = s.charAt(i);
+    //         sb.append(c);
+    //         if(chack(sb.toString())){
+    //             count++;
+    //             sb.setLength(0);
+    //         }
             
-        }
-        return count;
-    }
+    //     }
+    //     return count;
+    // }
 
-    public static boolean chack(String s){
-        int l = 0;
-        int r = 0;
-        for(int i =0;i<s.length();i++){
-            if(s.charAt(i) == 'R')
-                r++;
-            else
-                l++;
-        }
+    // public static boolean chack(String s){
+    //     int l = 0;
+    //     int r = 0;
+    //     for(int i =0;i<s.length();i++){
+    //         if(s.charAt(i) == 'R')
+    //             r++;
+    //         else
+    //             l++;
+    //     }
 
-        return r == l;
-    }
+    //     return r == l;
+    // }
 }
