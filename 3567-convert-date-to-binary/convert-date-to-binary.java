4bb1bafac21b05404ddeddc154159f50a31eb5ee
Mokhanes A <mokhanes007@gmail.com>
2025-09-14 23:03:00 +0530

    Time: 2 ms (92.59%) | Memory: 42.2 MB (99.37%) - LeetSync

--- 3567-convert-date-to-binary/convert-date-to-binary.java
+++ 3567-convert-date-to-binary/convert-date-to-binary.java
@@ -1,23 +1,41 @@
+// class Solution {
+//     public String convertDateToBinary(String date) {
+//         int year = Integer.parseInt(date.substring(0, 4));
+//         int month = Integer.parseInt(date.substring(5, 7));
+//         int day = Integer.parseInt(date.substring(8, 10));
+
+//         String yearBin = toBinary(year);
+//         String monthBin = toBinary(month);
+//         String dayBin = toBinary(day);
+
+//         return yearBin + "-" + monthBin + "-" + dayBin;
+//     }
+
+//     private String toBinary(int n) {
+//         if (n == 0) return "0";
+//         StringBuilder sb = new StringBuilder();
+//         while (n > 0) {
+//             sb.append(n % 2);   // store remainder
+//             n /= 2;             // divide by 2
+//         }
+//         return sb.reverse().toString();
+
+        
+//     }
+// }
+
+
 class Solution {
     public String convertDateToBinary(String date) {
-        int year = Integer.parseInt(date.substring(0, 4));
-        int month = Integer.parseInt(date.substring(5, 7));
-        int day = Integer.parseInt(date.substring(8, 10));
-
-        String yearBin = toBinary(year);
-        String monthBin = toBinary(month);
-        String dayBin = toBinary(day);
-
-        return yearBin + "-" + monthBin + "-" + dayBin;
-    }
-
-    private String toBinary(int n) {
-        if (n == 0) return "0";
-        StringBuilder sb = new StringBuilder();
-        while (n > 0) {
-            sb.append(n % 2);   // store remainder
-            n /= 2;             // divide by 2
-        }
-        return sb.reverse().toString();
+       String y = date.substring(0,4);
+       String m = date.substring(5,7);
+       String d = date.substring(8,10);
+       StringBuilder sb = new StringBuilder();
+       sb.append(Integer.toBinaryString(Integer.parseInt(y)));
+       sb.append("-");
+       sb.append(Integer.toBinaryString(Integer.parseInt(m))); 
+       sb.append("-");
+       sb.append(Integer.toBinaryString(Integer.parseInt(d)));
+       return sb.toString(); 
     }
 }
