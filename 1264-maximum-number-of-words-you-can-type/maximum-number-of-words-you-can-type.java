6663f7581db812bb6d8478e213b9f7e71531aaac
Mokhanes A <mokhanes007@gmail.com>
2025-09-15 10:08:40 +0530

    Time: 1 ms (99.30%) | Memory: 42.2 MB (73.09%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
@@ -1,19 +1,44 @@
 class Solution {
-    public int canBeTypedWords(String text, String brokenLetters) {
-        String[] parts = text.split(" ");
-        char b[] = brokenLetters.toCharArray();
-        int count = 0;
-        for(String s : parts){
-            boolean flag = true;
-            for(char c : b){
-                if(s.contains(""+c)){
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) count++;
-        } 
+    // public int canBeTypedWords(String text, String brokenLetters) {
+    //     String[] parts = text.split(" ");
+    //     char b[] = brokenLetters.toCharArray();
+    //     int count = 0;
+    //     for(String s : parts){
+    //         boolean flag = true;
+    //         for(char c : b){
+    //             if(s.contains(""+c)){
+    //                 flag = false;
+    //                 break;
+    //             }
+    //         }
+    //         if(flag) count++;
+    //     } 
 
+    //     return count;
+    // }
+
+    public int canBeTypedWords(String text, String brokenLetters) {
+        String[] arr = text.split(" ");
+        char[] brokenWords = brokenLetters.toCharArray();
+        boolean[] charExists = new boolean[26];
+        for (char c : brokenWords) {
+            charExists[c - 'a'] = true;
+        }
+        int count = 0;
+        for (String word : arr) {
+            if (!hasBrokenLetters(word, charExists)) {
+                count++;
+            }
+        }
         return count;
+    }
+
+    public boolean hasBrokenLetters(String s, boolean[] charExists) {
+        for (int i = 0; i < s.length(); i++) {
+            if (charExists[s.charAt(i) - 'a']) {
+                return true;
+            }
+        }
+        return false;
     }
 }
