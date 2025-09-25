c789a10c174d31646da9839733a0cb76c13ad6d4
Mokhanes A <mokhanes007@gmail.com>
2025-09-25 17:17:34 +0530

    Time: 2 ms (95.44%) | Memory: 50.5 MB (15.73%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.java
+++ 485-max-consecutive-ones/max-consecutive-ones.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i =0;i<nums.length;i++){
             if(nums[i]==1){
                 count++;
-                max = Math.max(max,count);
+                max = max<count ? count : max;
             }else{
                 count = 0;
             }
