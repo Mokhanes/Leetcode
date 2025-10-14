957d602a18240bfab77a75b4d6618c8c65d1c0fc
Mokhanes A <mokhanes007@gmail.com>
2025-10-14 17:51:30 +0530

    Time: 1 ms (99.83%) | Memory: 44.8 MB (24.51%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,18 +1,42 @@
+// class Solution {
+//     public int[] smallerNumbersThanCurrent(int[] nums) {
+//         int n = nums.length;
+//         int res[] = new int[n];
+
+//         for(int i =0;i<n;i++){
+//             int c = 0;
+//             for(int j =0;j<n;j++){
+//                 if(nums[i] > nums[j]){
+//                     c++;
+//                 }
+//             }
+//             res[i] = c;
+//         }
+
+//         return res;
+//     }
+// }
+
+
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int n = nums.length;
-        int res[] = new int[n];
-
-        for(int i =0;i<n;i++){
-            int c = 0;
-            for(int j =0;j<n;j++){
-                if(nums[i] > nums[j]){
-                    c++;
-                }
-            }
-            res[i] = c;
+        
+        int[] freq = new int[101];
+        for (int el : nums) {
+            freq[el]++;
         }
 
-        return res;
+        
+        for (int i = 1; i < freq.length; i++) {
+            freq[i] = freq[i - 1] + freq[i];
+        }
+
+        
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+           ans[i] = nums[i] == 0 ? 0 : freq[nums[i] - 1];
+        }
+
+        return ans;
     }
 }
