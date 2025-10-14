ae5f59d0eaf43f212d5ac99e083fef7561fcbdd9
Mokhanes A <mokhanes007@gmail.com>
2025-10-14 18:21:10 +0530

    Time: 7 ms (100.00%) | Memory: 45.8 MB (12.60%) - LeetSync

--- 1708-design-parking-system/design-parking-system.java
+++ 1708-design-parking-system/design-parking-system.java
@@ -1,41 +1,17 @@
 class ParkingSystem {
 
-    int bigc[];
-    int midc[];
-    int smlc[];
+    int arr[] = new int[3];
 
     public ParkingSystem(int big, int medium, int small) {
-        bigc = new int[big];
-        midc = new int[medium];
-        smlc = new int[small];
+        this.arr[0] = big;
+        this.arr[1] = medium;
+        this.arr[2] = small;
     }
     
     public boolean addCar(int carType) {
-        if(carType == 1){
-            for(int i =0;i<bigc.length;i++){
-                if(bigc[i] == 0){
-                    bigc[i] = 1;
-                    return true;
-                }
-            }
-        }else if(carType == 2){
-            for(int i =0;i<midc.length;i++){
-                if(midc[i] == 0){
-                    midc[i] = 1;
-                    return true;
-                }
-            }
-        }else if(carType == 3){
-            for(int i =0;i<smlc.length;i++){
-                if(smlc[i] == 0){
-                    smlc[i] = 1;
-                    return true;
-                }
-            }
-        }
-
-        return false;
+        return --arr[--carType] >= 0;
     }
+        
 }
 
 /**
