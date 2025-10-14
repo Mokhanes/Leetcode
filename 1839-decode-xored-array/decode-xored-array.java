e0ecdc282a9a5119181f51ad764731a030690c25
Mokhanes A <mokhanes007@gmail.com>
2025-10-14 18:03:34 +0530

    Time: 2 ms (91.63%) | Memory: 46.1 MB (21.50%) - LeetSync

--- 1839-decode-xored-array/decode-xored-array.java
+++ 1839-decode-xored-array/decode-xored-array.java
@@ -1,13 +1,23 @@
 class Solution {
     public int[] decode(int[] encoded, int first) {
-        int n = encoded.length;
-        int res[] = new int[n+1];
+        // int n = encoded.length;
+        // int res[] = new int[n+1];
 
-        res[0] = first;
-        for(int i =1;i<n+1;i++){
-            res[i] = res[i-1]^encoded[i-1];
+        // res[0] = first;
+        // for(int i =1;i<n+1;i++){
+        //     res[i] = res[i-1]^encoded[i-1];
+        // }
+
+        // return res;
+
+
+        int[] arr = new int[encoded.length + 1];
+        arr[0] = first;
+        for (int i = 0; i < encoded.length; i++) {
+            arr[i + 1] = arr[i] ^ encoded[i];
         }
+        return arr;
 
-        return res;
+        
     }
 }
